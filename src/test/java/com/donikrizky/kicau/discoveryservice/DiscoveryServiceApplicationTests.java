f10687d93a530f44e39cbe8bd33ba00231ad52ea
Doni K Rizky <donikrizky@gmail.com>
2020-09-01 23:55:13 +0700

    initial commit

---
+++ src/test/java/com/donikrizky/kicau/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donikrizky.kicau.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
